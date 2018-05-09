a40c15d77fef22b469d99dc34d5d75080c5466eb
Samuel Ortega <samuelortega@iPhone.csusm.edu>
2018-05-09 11:47:05 -0700

    Updated link

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene ("Survival");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log ("QUIT");
+		Application.Quit ();
+	}
+	public void SetVolume(float volume)
+	{
+		Debug.Log (volume);
+
+	}
+}
